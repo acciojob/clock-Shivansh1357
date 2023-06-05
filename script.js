74f799b892b7e3f2871fbe77af5e0f8bb930ba70
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-05 10:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateTime() {
+      var now = new Date();
+      var timer = document.getElementById("timer");
+      timer.innerHTML = now.toLocaleString();
+    }
+
+    // Call updateTime function every second (1000 milliseconds)
+    setInterval(updateTime, 1000);
